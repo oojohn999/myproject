f238888f201a2824850e2f6c1eef08fba88d7527
操作者姓名 <email>
2020-02-23 17:22:19 +0800

    Initial commit

---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Person {
+    public  void  hello(){
+        System.out.println("Hello world!!!!!!!!! ");
+
+    }
+
+
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,18 @@
+package com.tom
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+
+
+}
+
+class Human{
+    fun hello(){
+
+        println("Hello kotlin")
+    }
+
+}
