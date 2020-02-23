90c320a163db9a7dd71662a2cdd54c6b5e89634b
操作者姓名 <email>
2020-02-23 20:10:01 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -4,6 +4,18 @@ public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
 
-        new Person().hello();
+       Person p= new Person();
+       p.hello();
+
+       int age;
+       age=22;
+       char c='中';
+       byte b=30;
+       float weight =65.5f;
+       boolean enroll=true;
+       String s=new String();
+       System.out.println(enroll);
+
+
     }
 }
